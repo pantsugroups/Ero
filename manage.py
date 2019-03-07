d273bc15b3803c90fc6c7f6b2e2b9b660446ff20
9bie <blackguwc@163.com>
2019-03-07 17:38:30 +0800

    test

--- manage.py
+++ manage.py
@@ -2,10 +2,11 @@
 from flask import Flask
 from flask_cors import *
 from app.novel import create
+from config import *
 app = Flask(__name__)
 # from app import novel
 CORS(app, supports_credentials=True)
 # app.register_blueprint(novel.app, url_prefix="/erolight")
 if __name__ == "__main__":
     app = create(app)
-    app.run()
+    app.run(port=WEB_PORT,host=WEB_ADDRESS)
