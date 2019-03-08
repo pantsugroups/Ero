45d98251d764b37d649050b60082f5dc60dbb6f7
9bie <blackguwc@163.com>
2019-03-08 21:04:25 +0800

    emmm

--- app/novel/conf/config.py
+++ app/novel/conf/config.py
@@ -6,6 +6,6 @@ SMTP_PASSWD = ''
 
 
 # 节点配置
-DOWNLOAD_REMOTE_SERVER = [  # 首个为主节点，必须和主站为同一路径
+DOWNLOAD_REMOTE_SERVER = [ "https://api.ero.ink/"  # 首个为主节点，必须和主站为同一路径
                           ]
 DL_SAVE_ADDRESS = '/root/Novel'
--- app/novel/stream/views.py
+++ app/novel/stream/views.py
@@ -59,13 +59,13 @@ def upload_volume():
 @stream.route("/download/<vid>")
 @login_required
 def download(vid ):
-    servers = int(request.args.get("servers"))
+    servers = request.args.get("servers")
     if not vid:
         return jsonresp({"code": -2, "msg": "缺少参数"})
     if not servers or int(servers) > config.DOWNLOAD_REMOTE_SERVER:
         servers = config.DOWNLOAD_REMOTE_SERVER[0]
     else:
-        servers = config.DOWNLOAD_REMOTE_SERVER[servers]
+        servers = config.DOWNLOAD_REMOTE_SERVER[int(servers)]
     try:
         links = models.Volume.get(models.Volume.id == vid).files
         token = generate_token(links[links.rfind("/") + 1:])
