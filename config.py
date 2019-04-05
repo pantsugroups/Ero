778a6859814d6aee0930c5be7c1707da505c22fa
TerraceCN <zky3213@gmail.com>
2019-04-05 11:38:12 +0800

    product

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ from peewee import SqliteDatabase
 
 HOSTNAME = "0.0.0.0"
 PORT = 5000
-DEBUG = True
 
 DB = SqliteDatabase("ero.db")
 
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import ero_api
 import config
 
 app = ero_api.create_app(config)
-app.run(host=config.HOSTNAME,
-        port=config.PORT,
-        debug=config.DEBUG)
+
+if __name__ == "__main__":
+    app.run(host=config.HOSTNAME,
+            port=config.PORT,
+            debug=True)
