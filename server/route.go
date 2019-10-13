d4a12663073f4b0bf55714adc3b119a433f50361
9bie <blackguwc@163.com>
2019-10-13 17:03:43 +0800

    add cors:option method

--- server/route.go
+++ server/route.go
@@ -29,7 +29,8 @@ func NewRouter() *echo.Echo {
 		AllowOrigins:     []string{"*"},
 		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
 		AllowCredentials: true,
-		AllowMethods:     []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+
+		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE, echo.OPTIONS},
 	}))
 	e.Use(middleware.Logger())
 
